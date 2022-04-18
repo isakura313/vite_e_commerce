8463ccf45f205cde2bb284c6bb405a191c5bac1c
Paul <isakura313@gmail.com>
2022-04-18 22:05:21 +0300

    add

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { store } from './store'
 import './assets/index.postcss'
 import '@fortawesome/fontawesome-free/css/all.css'
 import '@fortawesome/fontawesome-free/js/all.js'
+import countTo from 'vue3-count-to';
 
 const head = createHead()
 const app = createApp(App)
@@ -13,5 +14,6 @@ const app = createApp(App)
 app.use(store)
 app.use(router)
 app.use(head)
+app.use(countTo);
 
 app.mount('#app')
--- src/pages/CartMain.vue
+++ src/pages/CartMain.vue
@@ -14,17 +14,21 @@
         <span>Доступные способы и время доставки можно выбрать при оформлении заказа</span>
         <div><span>Итоговая сумма</span> <span class="font-bold"> {{ count }}</span></div>
       </div>
-      <h5 class="font-xl"> Итоговая сумма <span class="font-bold"> {{ sum }} </span></h5>
+      <h5 class="font-xl"> Итоговая сумма  <CountTo :startVal='0' :endVal='finalSum' :duration='300'></CountTo> </h5>
     </div>
+
     <div class="wrapper w-128 " v-else>
       <h1 class="text-2xl font-bold">Здесь ничего нет! Отправляйтесь за покупками</h1>
     </div>
+
+
   </div>
 </template>
 
 
 <script setup lang="ts">
 import {useStore, Mutation} from "@/store/index";
+import { CountTo } from 'vue3-count-to';
 import CartProduct from "../components/CartProduct.vue";
 import {computed, onMounted, watch, ref} from "vue";
 
@@ -33,16 +37,4 @@ const store = useStore();
 const cart = computed(() => store.getters.getCart);
 console.log(cart)
 const finalSum = computed(() => store.getters.finalSum);
-watch(finalSum, () => {
-  // alert("change")
-  const timer = setInterval(() => {
-    // for (let i = 0; i > 20; i++) {
-      sum.value+=10
-    // }
-    if(sum.value === finalSum.value){
-      clearInterval(timer)
-    }
-  }, 0.01)
-
-})
 </script>
