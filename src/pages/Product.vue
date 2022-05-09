5dc05db0c36a7da787ac365ca7dae052b9e4fe16
Paul <isakura313@gmail.com>
2022-05-09 22:22:34 +0300

    addd

--- src/pages/Product.vue
+++ src/pages/Product.vue
@@ -1,11 +1,24 @@
 <template>
   <div>
-    {{ productInfo.id }}
-    <vue-picture-swipe :items="items" class="gallery"></vue-picture-swipe>
+    <div class="flex justify-center">
+      <div v-if="loader" class="grid grid-cols-4 width-1/2 gap-16 pl-16 pr-16">
+        <vue-picture-swipe :items="items" class="gallery"></vue-picture-swipe>
+        <img :src="productInfo.img" width="400"/>
+        <div class="product_info">
+          <div v-for="(key,info) in details" >
+            <span class="text-grey-600">{{info}}</span> :
+            <span>{{key}}</span>
+          </div>
+        </div>
+        <div class="max-w-sm rounded shadow-lg h-64 ">
+          <h1 class="font-bold font-9xl">{{ productInfo.name }}</h1>
+          <button class="bg-indigo-500 hover:bg-indigo-700 text-white h-12 w-24 rounded-md">Заказать</button>
+          <button class="bg-pink-600 hover:bg-pink-700 text-white h-12 w-24 rounded-md">Заказать</button>
+        </div>
+      </div>
+    </div>
   </div>
-
 </template>
-
 
 
 <script setup lang="ts">
@@ -16,6 +29,9 @@ import {useRoute} from 'vue-router'
 import axios from 'axios';
 import {ref, computed, onMounted} from 'vue'
 import {Product} from '../types/';
+const details = ref('')
+
+const loader = ref(false);
 const route = useRoute()
 const items = ref([])
 const id = ref(route.params.id)
@@ -23,29 +39,57 @@ const productInfo = ref('')
 onMounted(async () => {
   const results = await axios({
     method: "GET",
-    url: `http://localhost:5000/products/${route.params.id}`
+    url: `http://localhost:5000/products/${route.params.id}`,
   });
   productInfo.value = results.data;
-  productInfo.value.imgSrc.map((item:string)=>{
+  details.value = results.data.details;
+  productInfo.value.imgSrc.map((item: string, index: number) => {
+    console.log(productInfo.value)
     items.value.push({
       src: item,
       thumbnail: item,
-      // w: 500,
-      h: 500,
-      alt: 'some numbers on a grey background'
+      w: productInfo.value.imgSizes[index].width,
+      h: productInfo.value.imgSizes[index].height,
+      alt: 'some numbers on a grey background',
     })
   })
-  // loader.value = true;
+  loader.value = true;
 })
 
 </script>
 
 <style>
-.gallery img{
-  height: 200px;
+.gallery {
+  width: 400px;
 }
-.my-gallery{
+
+.gallery img {
+  height: 300px;
+}
+
+.my-gallery {
+  border: 2px solid black;
   display: flex;
+  flex-direction: column;
+  width: 400px;
+}
+
+.pswp__top-bar {
+  background-color: #2c2d34 !important;
+}
+
+.gallery-thumbnail {
+  border: 2px solid #fff;
+  justify-content: center;
+
+}
+
+.gallery-thumbnail:hover {
+  border: 2px solid blue;
+}
+
+.pswp__item {
+  background-color: white;
 }
 
 </style>
